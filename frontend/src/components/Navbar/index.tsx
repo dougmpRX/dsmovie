b32dedbf5472308bf7b6135f8356ffa923fc2d74
Douglas MP <douglasmpestana@gmail.com>
2022-05-11 08:21:57 -0300

    Security config

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/dougmpRX" target="_blank" rel="noreferrer">
+          <a href="https://github.com/dougmpRX" target="_blank" rel="noopener noreferrer">
             <div className="dsmovie-contact-container">
               <GithubIcon />
               <p className="dsmovie-contact-link">/dougmpRX</p>
